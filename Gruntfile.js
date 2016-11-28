aea911e0f655fc8c050ea23289ca0dccab1ec157
Ameen Ross <a.ross@amdev.eu>
2016-11-28 15:36:05 +0100

    Add grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        phpcs: {
+            application: {
+                src: ['src/'],
+            },
+            options: {
+                bin: 'vendor/bin/phpcs',
+                extensions: 'php',
+                ignore: '/node_modules,/vendor',
+                standard: 'PSR2',
+            },
+        },
+        phplint: {
+            good: ['src/**/*.php'],
+        },
+        exec: {
+            apigen: 'vendor/apigen/apigen/bin/apigen generate -s src/ -d docs/ --template-theme=bootstrap',
+        },
+        githooks: {
+            all: {
+                'pre-commit': 'default',
+            },
+        },
+    });
+
+    // PHP code linter.
+    grunt.loadNpmTasks('grunt-phplint');
+    grunt.registerTask('lint', ['phplint:good']);
+
+    // PHP code sniffer.
+    grunt.loadNpmTasks('grunt-phpcs');
+    grunt.registerTask('cs', ['phpcs']);
+
+    // Apigen.
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('doc', ['exec:apigen']);
+
+    // Install git hooks.
+    grunt.loadNpmTasks('grunt-githooks');
+
+    // Default task, running linter and codesniffer.
+    grunt.registerTask('default', ['phplint:good', 'phpcs']);
+};
