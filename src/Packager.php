91ff7592ca09b5824cdbe72f2130caca0fa2d81f
Ameen Ross <a.ross@amdev.eu>
2016-11-28 18:01:20 +0100

    Start with the actual packaging code

--- src/Packager.php
+++ src/Packager.php
@@ -1,8 +1,44 @@
 <?php namespace AmeenRoss\MagentoPackager;
 
+use Archive_Tar as Tar;
+use SimpleXMLElement;
+
 class Packager
 {
-    public function __construct()
+    /**
+     * @var Tar $input
+     *     The source tarball to be packaged.
+     */
+    protected $input;
+
+    /**
+     * Create an extension packager.
+     *
+     * @param string $input
+     *     The filename of the tar source file.
+     * @param SimpleXMLElement $metadata
+     *     (optional) Object with the package and release metadata.
+     */
+    public function __construct($input, SimpleXMLElement $metadata = null)
     {
+        $this->input = new Tar($input);
+        $this->setMetadata($metadata);
+    }
+
+    /**
+     * Set the package's metadata.
+     *
+     * @param SimpleXMLElement $metadata
+     *     (optional) Object with the package and release metadata.
+     */
+    public function setMetadata(SimpleXMLElement $metadata = null)
+    {
+        if (isset($metadata)) {
+            // Store the given metadata on the object.
+            $this->metadata = $metadata;
+        } elseif (!isset($this->metadata)) {
+            // Instantiate an empty metadata object if needed.
+            $this->metadata = new SimpleXMLElement('<package/>');
+        }
     }
 }
